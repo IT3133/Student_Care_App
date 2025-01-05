a58b65ca47447b7d4287efdb7d3fbc2e921b24cb
Dinuki85 <prarthanadinuki2@gmail.com>
2025-01-05 23:25:48 +0530

    Set up dynamic navigation with BottomNavigation and scene rendering

--- StudentCareApp/components/Main.js
+++ StudentCareApp/components/Main.js
@@ -1,6 +1,23 @@
+import * as React from 'react';
 import { BottomNavigation } from "react-native-paper";
+import Profile from "./Profile";
+
+import Courses from "./Courses";
+import Subjects from "./Subjects";
 
 export default function Main({route}){
+    const {user} = route.params;
+    const [index, setIndex] = React.useState([
+        { key: 'profile', title: 'Profile', focusedIcon: 'account', unfocusedIcon: 'account-outline'},
+        { key: 'courses', title: 'Courses', focusedIcon: 'school' , unfocusedIcon: 'school-outline'},
+        { key: 'subjects', title: 'Subjects', focusedIcon: 'book-open' , unfocusedIcon: 'book-open-outline'}
+    ]);
+
+    const renderScene = BottomNavigation.SceneMap({
+        profile:()=> <Profile user={user}/>,
+        courses:()=> <Courses user={user}/>,
+        subjects:()=> <Subjects user={user}/>,
+      });
     return (
         <BottomNavigation navigationState={{index, routes }} onIndexChange={setIndex} renderScene={renderScene} keyExtractor={(item) => item.key}/>
     );
