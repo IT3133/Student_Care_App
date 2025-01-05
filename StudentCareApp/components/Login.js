5c64535cd7d2dead04d08f1a7db352044c2d5c99
Dinuki85 <prarthanadinuki2@gmail.com>
2025-01-05 20:30:30 +0530

    feat: add Login screen layout with React Native and Paper components

---
+++ StudentCareApp/components/Login.js
@@ -0,0 +1,30 @@
+import { Button, ScrollView, View } from "react-native";
+import { TextInput } from "react-native-paper";
+
+export default function Login(){
+    return(
+        <ScrollView contentContainerStyle={styles.scrollView}>
+            <View styles={styles.container}>
+                <View style = {styles.banner}>
+                    <Image style={styles.image} source={require('../assets/uovlogo.png')}/>
+
+                </View>
+                <View style ={ styles.title}>
+                    <text variant='displayMedium' style={styles.text}>Student Login</text>
+                </View>
+                <View style={styles.inputfields}>
+                    <TextInput label="Username" mode="outlined" activeOutlineColor="#63625b" value={username} onChangeText={setUserName}/>
+                    <TextInput label="Password" mode="outlined" activeOutlineColor="#63625b"  secureTextEntry value={password} onChangeText={setPassword}/>
+                    <Button style={styles.button} mode="contained" onPress={handleLogin} labelStyle={styles.buttonText}>Login</Button>
+                    {errorMessage ?(
+                        <Text style={styles.errorText}>{errorMessage}</Text>
+                    ) : null }
+
+                </View>
+                <View style={styles.footer}>
+                    <Text style={styles.footerText}>UoV © 2025</Text>
+                </View>
+            </View>
+        </ScrollView>
+    );
+}
